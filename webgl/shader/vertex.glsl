90d6614e930992f7e958b2a89befce449620703b
Higa <yh0331tigers@yahoo.co.jp>
2023-01-06 21:50:09 +0900

    modelViewMatrix, projectionMatrix実装

--- webgl/shader/vertex.glsl
+++ webgl/shader/vertex.glsl
@@ -35,5 +35,5 @@ void main(void) {
 
         vFinalColor = Ia + Id;
     }
-    gl_Position = vec4(aVertexPosition, 1.0);
+    gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition, 1.0);
 }
